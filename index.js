0439bd20a2578125a63a08cc8073de93cba82b23
owner <me>
2024-06-13 02:10:02 +0700

    add node_modules

---
+++ index.js
@@ -0,0 +1,9 @@
+// index.js
+// Define the port to listen on
+const port = 8080;
+
+// Start the server
+server.listen(port, () => {
+	console.log(`Server is running on http://localhost:${port}`);
+});
+
