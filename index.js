0d7aeac3c737c6442f5fea4f77eef92649d72a14
owner <me>
2024-06-16 08:50:29 +0700

    add github action: auto build at each commit

--- index.js
+++
@@ -1,9 +0,0 @@
-// index.js
-// Define the port to listen on
-const port = 8080;
-
-// Start the server
-server.listen(port, () => {
-	console.log(`Server is running on http://localhost:${port}`);
-});
-
